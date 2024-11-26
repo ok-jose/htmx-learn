791245b15a825af69be3b9d80ebe3872c122227e
Jose <lg282923166@gmail.com>
2024-11-26 19:58:34 +0800

    chore: root

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
+import { staticPlugin } from '@elysiajs/static';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(staticPlugin())
+  .listen(3000)
+  .get('/', () => 'Hello World!');
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+export { app };
